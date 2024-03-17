5668ab98fb367472b4c4abcfed1cd0dfd7b855c6
gunduz <gunduzabdullayev547@gmail.com>
2024-03-17 13:32:09 +0400

    Base Entities were added

---
+++ Core/HepsiBuradaApi.Domain/Entities/Category.cs
@@ -0,0 +1,28 @@
+﻿using HepsiBuradaApi.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiBuradaApi.Domain.Entities
+{
+    public class Category : EntityBase { 
+        public required int ParentId { get; set; }
+        public required string Name { get; set; }
+        public required int Priority { get; set; }
+        public ICollection<Detail> Details { get; set; }
+        public ICollection<Product> Products { get; set; }
+        public Category()
+        {
+            
+        }
+        public Category(int parentID,string name,int priority)
+        {
+            ParentId = parentID;
+            Name = name;
+            Priority = priority;
+        }
+
+    }
+}
