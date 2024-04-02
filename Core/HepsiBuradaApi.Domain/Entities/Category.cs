1300604cdfdb224bc106490dfe5116180ef2007e
gunduz <gunduzabdullayev547@gmail.com>
2024-04-03 00:01:10 +0400

    unitofworkadded

--- Core/HepsiBuradaApi.Domain/Entities/Category.cs
+++ Core/HepsiBuradaApi.Domain/Entities/Category.cs
@@ -8,9 +8,9 @@ using System.Threading.Tasks;
 namespace HepsiBuradaApi.Domain.Entities
 {
     public class Category : EntityBase { 
-        public required int ParentId { get; set; }
-        public required string Name { get; set; }
-        public required int Priority { get; set; }
+        public  int ParentId { get; set; }
+        public  string Name { get; set; }
+        public  int Priority { get; set; }
         public ICollection<Detail> Details { get; set; }
         public ICollection<Product> Products { get; set; }
         public Category()
