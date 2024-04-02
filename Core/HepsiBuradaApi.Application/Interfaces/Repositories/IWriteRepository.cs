18824176a62f7d3d01b3ccf5f51a6af522a3610f
gunduz <gunduzabdullayev547@gmail.com>
2024-04-02 23:23:14 +0400

    writerepofinished

--- Core/HepsiBuradaApi.Application/Interfaces/Repositories/IWriteRepository.cs
+++ Core/HepsiBuradaApi.Application/Interfaces/Repositories/IWriteRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HepsiBuradaApi.Domain.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace HepsiBuradaApi.Application.Interfaces.Repositories
 {
-    public interface IWriteRepository
+    public interface IWriteRepository<T> where T : class,IEntityBase, new()
     {
+        Task addAsync(T entity);
+        Task addRangeAsync(IList<T> entities);
+        Task<T> updateAsync(T entity);
+        Task hardDeleteAsync(T entity);
+
     }
 }
--- Infrastructure/HepsiBuradaApi.Persistance/Repositories/WriteRepository.cs
+++ Infrastructure/HepsiBuradaApi.Persistance/Repositories/WriteRepository.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using HepsiBuradaApi.Application.Interfaces.Repositories;
+using HepsiBuradaApi.Domain.Common;
+using HepsiBuradaApi.Persistance.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,34 @@ using System.Threading.Tasks;
 
 namespace HepsiBuradaApi.Persistance.Repositories
 {
-    public class WriteRepository
+    public class WriteRepository<T> : IWriteRepository<T> where T : class,IEntityBase,new()
     {
+        private readonly AppDbContext context;
+        public WriteRepository(AppDbContext dbcontext)
+        {
+            context = dbcontext;
+        }
+        private DbSet<T> Table { get => context.Set<T>(); }
+
+        public async Task addAsync(T entity)
+        {
+           await Table.AddAsync(entity);
+        }
+
+        public async Task addRangeAsync(IList<T> entities)
+        {
+            await Table.AddRangeAsync(entities);
+        }
+
+        public Task hardDeleteAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<T> updateAsync(T entity)
+        {
+           await Task.Run(() => Table.Update(entity));
+            return entity;
+        }
     }
 }
